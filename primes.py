c5cba8dbc67a49e560d73054ccc699bfaddfaade
Mohammed Fohpa <mfohpa@gmail.com>
2023-10-04 00:53:17 +0100

    first attempt at primes exercise

--- primes.py
+++ primes.py
@@ -3,4 +3,12 @@
 
 def primes(number_of_primes):
     list = []
+    primes = 2
+    
+    while len(list) != number_of_primes:
+        prime = True
+        for i in range (2, primes//2):
+            if primes % i == 0:
+                prime = False
+        list.append(i)
     return list
